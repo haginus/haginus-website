1c3dd1b3c1a5b98727d324c50ee446ab0da1abfc
Haginus <hagiandrei.ah@gmail.com>
2023-11-12 23:11:21 +0200

    add social links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.scss";
 import { getDominantColor } from "../lib/utils";
+import { Fab, Tooltip, Typography } from "@mui/material";
+import { GitHub, LinkedIn, Email, Phone } from "@mui/icons-material";
 
 export default function Header() {
 
@@ -27,8 +29,56 @@ export default function Header() {
           </h1>
           <h2 className="role">Software Engineer</h2>
           <h3 className="location">Bucharest, RO 🇷🇴</h3>
+          <div className="social">
+            <SocialLink 
+              title="GitHub" 
+              icon={<GitHub />} 
+              link="https://github.com/haginus"
+            />
+            <SocialLink 
+              title="LinkedIn"
+              icon={<LinkedIn />} 
+              link="https://linkedin.com/in/haginus"
+            />
+            <SocialLink 
+              title="Email"
+              icon={<Email />} 
+              text="hagiandrei.ah@gmail.com"
+              link="mailto:hagiandrei.ah@gmail.com"
+            />
+            <SocialLink 
+              title="Phone"
+              icon={<Phone />} 
+              text="+40764070525"
+              link="tel:+40764070525"
+            />
+          </div>
         </div>
       </div>
     </header>
+  )
+}
+
+interface SocialLinkProps {
+  title: string;
+  icon: React.ReactElement;
+  link: string;
+  text?: string;
+}
+
+function SocialLink({ title, icon, link, text }: SocialLinkProps) {
+  return (
+    <Tooltip title={title}>
+      <Fab
+        href={link}
+        target="_blank" 
+        size="small" 
+        variant={text ? 'extended' : 'circular'}
+        sx={{ height: 40 }}
+      >
+        {icon}
+        {text && <Typography sx={{ textTransform: 'none', ml: 0.25, fontSize: '0.8rem' }}>{text}</Typography>}
+      </Fab>
+    </Tooltip>
   )
 }
